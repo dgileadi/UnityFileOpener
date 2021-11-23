c53b614b5dcbaaf9217b9366d1ab5357c4606327
David Gileadi <gileadis@gmail.com>
2021-11-22 20:24:58 -0700

    Get it working on iOS

--- Plugins/iOS/OpenInPlace.mm
+++ Plugins/iOS/OpenInPlace.mm
@@ -1,11 +1,14 @@
 #import "OpenInPlace.h"
 
+static OpenInPlace *openInPlace;
+
 @implementation OpenInPlace
 
-+ (BOOL) OpenInPlace:(NSString *)url uti:(NSString *)uti name:(NSString *)name {
-    NSURL *url = [NSURL URLWithString:url];
+UIDocumentInteractionController* dic = nil;
 
-    UIDocumentInteractionController* dic = [UIDocumentInteractionController interactionControllerWithURL:url];
+- (BOOL) openInPlace:(NSString *)url uti:(NSString *)uti name:(NSString *)name {
+    dic = [UIDocumentInteractionController interactionControllerWithURL:[NSURL URLWithString:url]];
+    dic.delegate = self;
     if (uti != nil)
         dic.UTI = uti;
     if (name != nil)
@@ -13,9 +16,16 @@
     return [dic presentOpenInMenuFromRect:CGRectZero inView:UnityGetGLView() animated:YES];
 }
 
+- (void)documentInteractionController:(UIDocumentInteractionController *)controller
+        willBeginSendingToApplication:(NSString *)application {
+    [dic dismissMenuAnimated:NO];
+    [dic dismissMenuAnimated:NO];
+}
+
 extern "C" {
-    void UFO_OpenInPlace(const char* url, const char* uti, const char* name) {
-        [OpenInPlace OpenInPlace:[DataConvertor charToNSString:url] uti:[DataConvertor charToNSString:uti] name:[DataConvertor charToNSString:name]];
+    void UFO_OpenInPlace(char* url, char* uti, char* name) {
+        openInPlace = [[OpenInPlace alloc] init];
+        [openInPlace openInPlace:[DataConvertor charToNSString:url] uti:[DataConvertor charToNSString:uti] name:[DataConvertor charToNSString:name]];
     }
 }
 
