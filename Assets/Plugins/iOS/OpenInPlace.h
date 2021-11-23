2ec7aeb7e4870144456aa2cd4369c96c68163f5c
David Gileadi <gileadis@gmail.com>
2021-11-22 20:24:58 -0700

    Get it working on iOS

--- Assets/Plugins/iOS/OpenInPlace.h
+++ Assets/Plugins/iOS/OpenInPlace.h
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 #import "DataConvertor.h"
 
-@interface OpenInPlace : NSObject
+@interface OpenInPlace : NSObject <UIDocumentInteractionControllerDelegate>
 
-+ (BOOL) OpenInPlace:(NSString *)url uti:(NSString *)uti name:(NSString *)name;
+- (BOOL) openInPlace:(NSString *)url uti:(NSString *)uti name:(NSString *)name;
 
 @end
